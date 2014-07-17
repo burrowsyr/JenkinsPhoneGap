5fabd2e178b9fa2a8e9a96f4bd332c29414b51a1
Ricky Clegg <me@rickyclegg.com>
2014-07-17 10:27:02 +0100

    Created main view

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,49 +1,27 @@
 Ext.define('JenkinsPhoneGap.view.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.Panel',
     xtype: 'main',
     requires: [
         'Ext.TitleBar',
-        'Ext.Video'
+        'Ext.Button'
     ],
     config: {
-        tabBarPosition: 'bottom',
+        layout: {
+            type: 'vbox',
+            align: 'center'
+        },
+
+        padding: 20,
 
         items: [
             {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'In App Browsing'
             },
             {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
+                xtype: 'button',
+                text: 'Google.co.uk'
             }
         ]
     }
